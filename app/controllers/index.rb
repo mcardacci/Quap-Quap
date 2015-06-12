bc9ba9915f9489893cee3fecb07f3f6d38a4dc4d
Denny Tek <tek.denny@gmail.com>
2015-06-12 16:55:49 -0400

    Add welcome/index and signup/login buttons

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,5 @@
 get '/' do
-  @current_user = User.all.first.quaps.first.picture.picture_url
+  # @current_user = User.all.first.quaps.first.picture.picture_url
   # Answer = @current_user.quaps
   erb :index
 end
