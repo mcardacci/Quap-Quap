f666800ca62fd6d3f9e4f1590119a065c42ea9ad
Apprentice <michaelbbozza@gmail.com>
2015-06-12 16:15:38 -0400

    added migrations and models

---
+++ db/migrate/20150612142828_create_quaps.rb
@@ -0,0 +1,11 @@
+class CreateQuaps < ActiveRecord::Migration
+  def change
+    create_table :quaps do |t|
+      t.belongs_to :answer, null: false
+      t.belongs_to :question, null: false
+      t.belongs_to :picture, null: false
+      t.belongs_to :user, null: false
+      t.string     :status, default: nil
+    end
+  end
+end
