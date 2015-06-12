c4366b50cdae48411b66d7aa11ea4ed70e05303d
mcardacci <marc.cardacci@gmail.com>
2015-06-12 19:39:09 -0400

    unfinished game controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ Answer.create(answer: "tek 9")
 Picture.create(picture_url: 'pictures/Cat.jpg')
 Quap.create(question_id: 1, answer_id: 1, picture_id: 1, user_id: 1)
 
-20.times do |num|
+3.times do |num|
   Quap.create(question: Question.create(question: Faker::Company.bs),
    answer: Answer.create(answer: Faker::Company.catch_phrase),
    picture: Picture.create(picture_url: Faker::Company.logo),
