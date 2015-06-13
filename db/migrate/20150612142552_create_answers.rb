3f2ddc9f68860fee8589e802cd7e1d1445b1841b
mcardacci <marc.cardacci@gmail.com>
2015-06-13 12:44:01 -0400

    change naming in migration fields

--- db/migrate/20150612142552_create_answers.rb
+++ db/migrate/20150612142552_create_answers.rb
@@ -1,7 +1,7 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
-      t.string :answer, null: false
+      t.string :body, null: false
       t.timestamps
     end
   end
