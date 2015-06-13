c3b8403bf3cb7da33c55d7772797c6d040c98a9e
Michael Bozza <michaelbbozza@gmail.com>
2015-06-13 15:07:08 -0400

    added counter for correct and incorrect at end of game

--- app/controllers/game_over.rb
+++ app/controllers/game_over.rb
@@ -1,3 +1,5 @@
 get "/game_over" do
+  @correct = Quap.where(status: "correct").count
+  @incorrect = Quap.where(status: "incorrect").count
   erb :'game/game_over'
 end
--- app/controllers/user_profile_controller.rb
+++ app/controllers/user_profile_controller.rb
@@ -1,5 +1,5 @@
 get "/user/:id" do
   current_user = User.find_by(id: session[:user_id])
-
+  reject_hacker
   erb :'user/user_profile', locals:{user: current_user}
 end
