f3503badf6ab39526351e32ff6c36da2c95115df
mcardacci <marc.cardacci@gmail.com>
2015-06-14 16:04:58 -0400

    adds users in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,8 @@
+names = %w(marco denny mike ben)
 
-# User.create(name: "denny", password: "cat")
-# Question.create(body: "What is muh name?")
-# Answer.create(body: "tek 9")
-# Picture.create(path: 'pictures/Cat.jpg')
-# Quap.create(question_id: 1, answer_id: 1, picture_id: 1, user_id: 1)
-
-# User.create(name: "marco", password: "123")
-# Question.create(body: "Color?")
-# Answer.create(body: "blue")
-# Picture.create(path: 'pictures/Cat.jpg')
-# Quap.create(question_id: 2, answer_id: 2, picture_id: 1, user_id: 2)
-
-# User.create(name: "mike", password: "123")
-# Question.create(body: "Structure?")
-# Answer.create(body: "Pillar")
-# Picture.create(path: 'pictures/Cat.jpg')
-# Quap.create(question_id: 3, answer_id: 3, picture_id: 1, user_id: 3)
-
-# User.create(name: "ben", password: "123")
-# Question.create(body: "Book?")
-# Answer.create(body: "neuromancer")
-# Picture.create(path: 'pictures/Cat.jpg')
-# Quap.create(question_id: 4, answer_id: 4, picture_id: 1, user_id: 4)
+4.times do |i|
+  User.create(name: names[i], password: "123")
+end
 
 File.open("db/chinese.rb", "r") do |f|
   f.each_line do |line|
