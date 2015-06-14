ec1f4c366e26fe6afc6c0c91fd49c12c14587b23
Michael Bozza <michaelbbozza@gmail.com>
2015-06-14 18:53:33 -0400

    fixed login issue

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -4,15 +4,12 @@ end
 
 post "/login" do
   @user = User.find_by(name: params[:user][:name])
-  return [500, "Invalid User"] unless @user
+  redirect "/login?invalid=invalid_user_or_password" unless @user
     if @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
       redirect "/user/#{@user.id}"
-    else
-     erb :'login_logout/logout'
     end
 end
-
 
 get "/logout" do
   session[:user_id] = nil
