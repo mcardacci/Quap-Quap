77277b0a785a45d4b999668d12102514ac84ab29
mcardacci <marc.cardacci@gmail.com>
2015-06-17 19:10:24 -0400

    adds ajax

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  $('.edit-user').on('click', updateUserForm);
+});
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+var updateUserForm = (function(event) {
+  event.preventDefault();
+  $.ajax({
+    url : "/user/:id/edit"
+  }).done(function(data){
+	var form = $(data).find('.edit-user-form').html();
+	var title = "<h1 class='update-title'>Update Your Profile!</h1>"
+    if ($('form').length) {
+      $('.update-title, form').remove();
+    } else {
+      $('.begin_game').append(title, form);
+    }
+  }).fail(function(){
+    alert("something's wrong")
+  })
 });
