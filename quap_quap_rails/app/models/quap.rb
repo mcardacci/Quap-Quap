d9de979048ce65d283ccfd09de249cec9199e444
mcardacci <marc.cardacci@gmail.com>
2015-09-18 14:49:18 -0400

    quap model methods

--- quap_quap_rails/app/models/quap.rb
+++ quap_quap_rails/app/models/quap.rb
@@ -4,7 +4,33 @@ class Quap < ActiveRecord::Base
   belongs_to :picture
   belongs_to :user
 
-  def method_name
-  	
+  def self.choices
+    correct_answer = self.where(status: nil).sample
+    if correct_answer == nil
+      return nil
+    else
+      incorrect_answers = self.where.not(id: correct_answer.id).sample(3)
+      questions = []
+      questions << correct_answer
+      questions << incorrect_answers
+      questions.flatten
+    end
+  end
+
+  def self.correct(input)
+    answer = self.find_by(answer_id: input)
+    answer.update(status: "correct")
+  end
+
+  def self.incorrect(input)
+    answer = self.find_by(answer_id: input)
+    answer.update(status: "incorrect")
+  end
+
+  def self.metrics
+    metrics = {}
+    metrics[:total] = self.all.count
+    metrics[:current] = self.all.count - self.where(status: nil).count + 1
+    return metrics
   end
 end
